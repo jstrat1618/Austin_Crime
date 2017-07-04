731e0f9abbdccce53cc9538a90b008713308e8c7
Justin <jstrat1.618@gmail.com>
2017-07-04 14:35:58 -0600

    Look at crime by zip more

--- Crime_by_Housing_Zip.R
+++ Crime_by_Housing_Zip.R
@@ -1,14 +1,41 @@
 library(readr)
 
-hzip15 <- read_csv('./dash-austin-crime-report-2015/2014_Housing_Market_Analysis_Data_by_Zip_Code.csv')
+hzip <- read_csv('cleaned_hzip.csv',
+                 col_types = cols(`Zip Code` = col_character()))
 dat <- read_csv('cleaned_data.csv',
                 col_types = cols(Zip_Code_Crime = col_character(),
                                  Zip_Code_Housing = col_character()))
 
+#Make sure Zip_Code_Crime and Zip_Code_Housing are always the same
+all(dat$Zip_Code_Crime == dat$Zip_Code_Housing, na.rm = TRUE)
 
+#Aside from NA's looks like they are
+one <- is.na(dat$Zip_Code_Crime) & !is.na(dat$Zip_Code_Housing)
+two <- is.na(dat$Zip_Code_Housing) &! is.na(dat$Zip_Code_Crime)
+any(one)
+any(two)
+
+#Sometime Zip_Code_Crime is not missing while Zip_Code_Housing is 
 dat %>%
-  group_by(Zip_Code_Housing) %>%
+  select(Zip_Code_Housing, Zip_Code_Crime, Highest_NIBRS_UCR_Offense_Description) %>%
+  filter(two)
+  
+
+#
+dat %>%
+  rename(`Zip` = Zip_Code_Crime) %>%
+  group_by(Zip) %>%
   summarise(Num_Crimes = n()) -> dat_zip
+
+#Not all the Zips match?
+all(hzip$`Zip Code` %in% dat_zip$Zip) #All match here but
+all(dat_zip$Zip %in% hzip$`Zip Code`) #but not here
+dat_zip$Zip[!dat_zip$Zip %in% hzip$`Zip Code`]#These zips aren't found
+
+
+hzip %>%
+  rename(`Zip` = `Zip Code`) %>%
+  full_join(dat_zip, by = "Zip") -> hzip_joined
 
 #Create housing zip code crime score
 ucr <- unique(dat$Highest_NIBRS_UCR_Offense_Description)
