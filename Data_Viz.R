78a83a84518aa6730931b9398ea1d33b680deff7
Justin <jstrat1.618@gmail.com>
2017-07-11 19:48:29 -0600

    Visualize data better

--- Data_Viz.R
+++ Data_Viz.R
@@ -2,13 +2,14 @@ library(readr)
 library(ggplot2)
 library(ggthemes)
 library(dplyr)
+library(lubridate)
 library(ggmap)
 
 hzip <- read_csv('cleaned_hzip.csv',
                  col_types = cols(`Zip Code` = col_character()))
 dat <- read_csv('cleaned_data.csv',
                 col_types = cols(Zip_Code_Crime = col_character(),
-                                 Zip_Code_Housing = col_character,
+                                 Zip_Code_Housing = col_character(),
                                  Clearance_Date = col_date(format = "%d-%b-%y")))
 
 dat %>%
@@ -56,7 +57,82 @@ ggmap(amap11)+
   facet_wrap(~Highest_NIBRS_UCR_Offense_Description)+
   labs(x = "Longitude", y = "Lattitude", title = 'Crime in Austin, TX')
 
-#Let's look at Crime over time
-dat$Clearance_Date
+#By Clearance status
+df_no_missing %>%
+  ggplot(aes(X_Coordinate, Y_Coordinate))+
+    geom_point(aes(X_Coordinate, Y_Coordinate), alpha = 0.2, shape = 1)+
+    geom_density2d()+
+    labs(x = "Longitude", y = "Lattitude", title = 'Crime in Austin, TX by Clearance Status')+
+    facet_wrap(~Clearance_Status)
+    theme_base()
 
+#Overlay this with the city
+ggmap(amap11)+
+    geom_point(aes(X_Coordinate, Y_Coordinate), data = df_no_missing,
+              alpha = 0.2, shape = 1)+
+    geom_density2d(aes(X_Coordinate, Y_Coordinate), data = df_no_missing)+
+    facet_wrap(~Clearance_Status)+
+    labs(x = "Longitude", y = "Lattitude", title = 'Crime in Austin, TX')
+    
+
+#Let's look at Crime over time
+dat %>%
+  select(Clearance_Date) %>%
+  filter(complete.cases(.))%>%
+  group_by(Clearance_Date) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(Clearance_Date, Count))+
+    geom_line()
+#There seems to be a large downward trend after Jan 2016
+#Also, there seems be be very a cyclical pattern, perhaps this is due to the weekday?
+dat %>%
+  select(Clearance_Date) %>%
+  filter(complete.cases(.))%>%
+  mutate(Weekday = wday(Clearance_Date, label = TRUE)) %>%
+  group_by(Weekday) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(Weekday, Count))+
+    geom_bar(stat = 'identity', width = 0.5)
+
+#There is considerably less crime on the weekend. 
+#Perhaps this may be due to data collection or the weekends are just that much more violent
+dat %>%
+  select(Clearance_Date) %>%
+  filter(complete.cases(.))%>%
+  mutate(Month = month(Clearance_Date, label = TRUE)) %>%
+  group_by(Month) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(Month, Count))+
+    geom_bar(stat = 'identity', width = 0.5)
+#Doesn't appear to be that much variation in month 
+
+#Let's look at the variation in type of crime
+dat %>%
+  select(Clearance_Date, Highest_NIBRS_UCR_Offense_Description) %>%
+  rename(`Offense` = Highest_NIBRS_UCR_Offense_Description) %>%
+  filter(complete.cases(.)) %>%
+  group_by(Clearance_Date, Offense) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(Clearance_Date, Count, col = Offense))+
+    geom_line()
+
+#Let's take out Theft
+dat %>%
+  select(Clearance_Date, Highest_NIBRS_UCR_Offense_Description) %>%
+  rename(`Offense` = Highest_NIBRS_UCR_Offense_Description) %>%
+  filter(complete.cases(.) & !(Offense %in% c("Theft", "Burglary", "Auto Theft"))) %>%
+  group_by(Clearance_Date, Offense) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(Clearance_Date, Count, col = Offense))+
+    geom_line()
+
+#Let's eliminate Theft, Burglary and Auto Theft
+dat %>%
+  select(Clearance_Date, Highest_NIBRS_UCR_Offense_Description) %>%
+  rename(`Offense` = Highest_NIBRS_UCR_Offense_Description) %>%
+  filter(complete.cases(.) & !(Offense %in% c("Theft", "Burglary", "Auto Theft"))) %>%
+  group_by(Clearance_Date, Offense) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(Clearance_Date, Count, col = Offense))+
+    geom_line()
 
